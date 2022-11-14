b688bf11264ccef46e31fba007477397c68e7918
FrismanGH <spidermansebik@seznam.cz>
2022-11-14 12:51:11 +0100

    14.11

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,24 @@
 import "./css/ExpenseItem.css";
+import React, { useState } from "react";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}czk</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
